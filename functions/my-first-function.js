56f8eb3fea31e714ab6c774fa82bae029ced0418
georgemirandajr <georgemirandajr@gmail.com>
2022-09-14 20:35:52 -0700

    key

--- functions/my-first-function.js
+++ functions/my-first-function.js
@@ -1,4 +1,22 @@
 exports.handler = async () => ({
     statusCode: 200,
-    body: 'boop',
+    body: {
+        API_URL:"https://api.collectapi.com/gasPrice/fromCity?city=boston",
+        API_KEY:'6JBsufP4GegihSRoEDsEsB:7i0kqhMWLNYcfmfMJReaaS'
+    },
   });
+
+
+// async function getCityGasPrice () {
+//     const response = await fetch( API_URL, { 
+//         headers: {
+//             'Content-type': 'application/json',
+//             'Authorization': `apikey ${API_KEY}`
+//         }
+//     } );
+//     const json = await response.json();
+//     console.log( json );
+// };
+
+// getCityGasPrice();
+
