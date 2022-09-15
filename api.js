85b7af6b70d4f99a038adc2497c14fc1e9269fc7
georgemirandajr <georgemirandajr@gmail.com>
2022-09-14 21:31:27 -0700

    all in key

--- api.js
+++ api.js
@@ -1,16 +1,13 @@
 API_URL = "https://api.collectapi.com/gasPrice/fromCity?city=boston"
 
-async function getKey() {
-    const response = await fetch( 'https://harmonious-cuchufli-93ffeb.netlify.app/.netlify/functions/my-first-function' );
-    const newKey = await response.json();
-    console.log( newKey );
-};
-
 async function getCityGasPrice () {
+    const fetchedKey = await fetch( 'https://harmonious-cuchufli-93ffeb.netlify.app/.netlify/functions/my-first-function' );
+    const keyString = await fetchedKey.json();
+    
     const response = await fetch( API_URL, { 
         headers: {
             'Content-type': 'application/json',
-            'Authorization': `apikey ${newKey}`
+            'Authorization': `apikey ${keyString}`
         }
     } );
     const json = await response.json();
