442985532948e235b5da0ec4c57f5b54e1a4c772
georgemirandajr <georgemirandajr@gmail.com>
2022-09-14 21:15:11 -0700

    removed async func from api func

--- functions/my-first-function.js
+++ functions/my-first-function.js
@@ -4,18 +4,3 @@ exports.handler = async () => ({
     statusCode: 200,
     body: { GAS_KEY },
   } );
-
-
-// async function getCityGasPrice () {
-//     const response = await fetch( API_URL, { 
-//         headers: {
-//             'Content-type': 'application/json',
-//             'Authorization': `apikey ${API_KEY}`
-//         }
-//     } );
-//     const json = await response.json();
-//     console.log( json );
-// };
-
-// getCityGasPrice();
-
