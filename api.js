7836409845b69d75e7ba27b51f671a539a07f008
georgemirandajr <georgemirandajr@gmail.com>
2022-09-14 21:27:48 -0700

    fetch key then use

--- api.js
+++ api.js
@@ -1,10 +1,16 @@
 API_URL = "https://api.collectapi.com/gasPrice/fromCity?city=boston"
 
+async function getKey() {
+    const response = await fetch( 'https://harmonious-cuchufli-93ffeb.netlify.app/.netlify/functions/my-first-function' );
+    const newKey = await response.json();
+    console.log( newKey );
+};
+
 async function getCityGasPrice () {
     const response = await fetch( API_URL, { 
         headers: {
             'Content-type': 'application/json',
-            'Authorization': `apikey ${GAS_KEY}`
+            'Authorization': `apikey ${newKey}`
         }
     } );
     const json = await response.json();
