13ecef983741bf20aef2a1060da343ac04fa6ae5
georgemirandajr <georgemirandajr@gmail.com>
2022-09-16 15:10:32 -0700

    fetching api

--- functions/my-first-function.js
+++ functions/my-first-function.js
@@ -1,6 +1,23 @@
 const { GAS_KEY } = process.env;
+API_URL = "https://api.collectapi.com/gasPrice/fromCity?city=boston";
 
-exports.handler = async () => ({
-    statusCode: 200,
-    body: GAS_KEY ,
-  } );
+exports.handler = async (event) => {
+    
+    const response = await fetch(API_URL, {
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `apikey ${GAS_KEY}`
+        }
+    });
+
+    const json = await response.json();
+    console.log(json);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data: GAS_KEY
+        }), 
+    };
+};
+    
