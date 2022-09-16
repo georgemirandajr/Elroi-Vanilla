f45e938a06d69a75d50d06b043675be5efb6aac3
georgemirandajr <georgemirandajr@gmail.com>
2022-09-15 20:50:41 -0700

    idk

--- api.js
+++ api.js
@@ -1,16 +1,15 @@
 API_URL = "https://api.collectapi.com/gasPrice/fromCity?city=boston"
-// const { GAS_KEY } = process.env;
-import {GAS_KEY} from './.netlify/functions/my-first-function'
+const { GAS_KEY } = process.env;
 
-async function getCityGasPrice () {
-    const response = await fetch( API_URL, { 
+async function getCityGasPrice() {
+    const response = await fetch(API_URL, {
         headers: {
             'Content-type': 'application/json',
             'Authorization': `apikey ${GAS_KEY}`
         }
-    } );
+    });
     const json = await response.json();
-    console.log( json );
+    console.log(json);
 };
 
 getCityGasPrice();
